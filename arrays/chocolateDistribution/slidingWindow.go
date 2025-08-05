d055511de4880ea3eb06cc155c09071e468732bd
devmanishoffl <devmanishofficial@gmail.com>
2025-08-05 09:22:16 +0000

    added array to chocholateDistribution problem

--- arrays/chocolateDistribution/slidingWindow.go
+++ arrays/chocolateDistribution/slidingWindow.go
@@ -5,26 +5,28 @@ import (
 	"sort"
 )
 
-func slidingWindow(arr []int, m int) int {
+func slidingWindow(arr []int, m int) (int, []int) {
 	sort.Ints(arr)
 	n := len(arr)
 
 	if n == 0 || n < m {
-		return 0
+		return 0, []int{}
 	}
 
 	minDiff := arr[m-1] - arr[0]
+	st, end := 0, m-1
 
-	for i := 1; i <= n-m; i++ {
+	for i := 0; i < n-m+1; i++ {
 		diff := arr[i+m-1] - arr[i]
 		if diff < minDiff {
 			minDiff = diff
+			st, end = i, i+m-1
 		}
 	}
-	return minDiff
+	return minDiff, arr[st : end+1]
 }
 
 func main() {
 	arr := []int{12, 4, 7, 9, 2, 23, 25, 41, 30, 40, 28, 42, 30, 44, 48, 43, 50}
-	fmt.Println(slidingWindow(arr, 7))
+	fmt.Println(slidingWindow(arr, 8))
 }
--- toPractice/main.go
+++ toPractice/main.go
@@ -5,27 +5,29 @@ import (
 	"sort"
 )
 
-func slidingWindow(arr []int, m int) int {
+func distribution(arr []int, m int) (int, []int) {
 	sort.Ints(arr)
 	n := len(arr)
 
-	if m == 0 || m > n {
-		return 0
+	if n == 0 || n < m {
+		return 0, []int{}
 	}
 
-	minDiff := arr[m-1] - arr[0]
+	assumeMinDiff := arr[m-1] - arr[0]
+	st, end := 0, m-1
 
-	for i := 1; i <= n-m; i++ {
+	for i := 0; i <= n-m; i++ {
 		diff := arr[i+m-1] - arr[i]
-		if diff < minDiff {
-			minDiff = diff
+		if diff < assumeMinDiff {
+			assumeMinDiff = diff
+			st = i
+			end = i + m - 1
 		}
 	}
-
-	return minDiff
+	return assumeMinDiff, arr[st : end+1]
 }
 
 func main() {
 	arr := []int{12, 4, 7, 9, 2, 23, 25, 41, 30, 40, 28, 42, 30, 44, 48, 43, 50}
-	fmt.Println(slidingWindow(arr, 7))
+	fmt.Println(distribution(arr, 9))
 }
