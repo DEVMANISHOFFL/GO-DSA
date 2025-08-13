1c3e776598968757aae4291103d4a76e589fdcb5
devmanishoffl <devmanishofficial@gmail.com>
2025-08-13 11:43:58 +0000

    strings started

--- strings/isPalindrome/main.go
+++ strings/isPalindrome/main.go
@@ -6,21 +6,22 @@ import (
 )
 
 func isPalindrome(s string) bool {
-	left, right := 0, len(s)-1
+	l, r := 0, len(s)-1
 
-	for left < right {
-		for left < right && !unicode.IsLetter(rune(s[left])) && !unicode.IsDigit(rune(s[left])) {
-			left++
-		}
-		for left < right && !unicode.IsLetter(rune(s[right])) && !unicode.IsDigit(rune(s[right])) {
-			right--
+	for l < r {
+		for l < r && !unicode.IsLetter(rune(s[l])) && !unicode.IsDigit(rune(s[l])) {
+			l++
 		}
 
-		if unicode.ToLower(rune(s[left])) != unicode.ToLower(rune(s[right])) {
+		for l < r && !unicode.IsLetter(rune(s[r])) && !unicode.IsDigit(rune(s[r])) {
+			r--
+		}
+
+		if unicode.ToLower(rune(s[l])) != unicode.ToLower(rune(s[r])) {
 			return false
 		}
-		left++
-		right--
+		l++
+		r--
 	}
 	return true
 }
